8b267e78c380047665a7040c815ec3e4689120d0
Aslam Rasheed <aslampr07@outlook.com>
2017-09-20 21:46:52 +0530

    bad commit

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
 
 var app = express();
-app.get("/", function (req, res) {
-    res.send("Hai How are you");
+app.use(bodyParser.json()); //may not need this line, as there is no json parsing.
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.post("/register", function (req, res) {
+    var username = req.body.username;
+    var email = req.body.email;
+    var password = req.body.password;
 });
 
 app.listen(8000);
