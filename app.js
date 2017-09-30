cdef1d13c24171395c418f174278269da80a79b2
Aslam Rasheed <aslampr07@outlook.com>
2017-09-30 11:08:22 +0530

    The login and token response has been setup.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var mysql = require('mysql');
 var path = require('path');
 var validator = require('validator');
+var crypto = require('crypto');
 
 //express initilization.
 var app = express();
@@ -47,6 +48,7 @@ app.post("/register", function (req, res) {
     //The sql query for inserting data
     else
     {
+        //The password is not salted yet, it should be done ASAP
         var data = { 'username': username, 'email': email, 'password': password, 'creationTime': new Date() };
         var sql = "INSERT INTO User SET ?";
         con.query(sql, data, function (err, result) {
@@ -87,8 +89,57 @@ app.get("/register", function (req, res) {
     res.sendFile(path.join(__dirname,"webview/register.html"));
 });
 
-app.get("/login", function (req, res) {
-    res.send("Working");
+app.post("/login", function (req, res) {
+    //I don't know if it is good idea to send the password over the post request,
+    //might need to change it.
+    var login = req.body.login;
+    var password = req.body.password;
+    var sql = mysql.format("SELECT password, ID from User where username = ? or email = ?", [login, login]);
+    con.query(sql, function (err, result, field) {
+        if (result.length == 0)
+        {
+            var response = {
+                'status': 'error',
+                'type': 'notExist'
+            };
+            res.send(response);
+        }
+        else
+        {
+            //This single lines compare the password, The gateway to everything.
+            if (result[0].password == password)
+            {
+                var token = crypto.randomBytes(20).toString('HEX');
+                var data = { 'userID': result[0].ID, 'token': token, 'creationTime': new Date() };
+                var sql = "INSERT INTO Authentication SET ?";
+                con.query(sql, data, function (err, result) {
+                    if (err)
+                    {
+                        res.send("There is a fucking error")
+                    }
+
+                    //When is everything is fine.
+                    else
+                    {
+                        var response = {
+                            'status': 'success',
+                            'token': token
+                        };
+                        res.send(response);
+                    }
+                })
+            }
+            else
+            {
+                var response = {
+                    'status': 'error',
+                    'type': 'wrongPassword'
+                };
+                res.send(response);
+            }
+        }
+
+    });
 });
 
 //The server is listening at port 8000, not using 80 because it need sudo for running the script, which is a
