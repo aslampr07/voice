dd8550aec8d06707b08ce9a93c5df3f696840b1f
Aslam Rasheed <aslampr07@outlook.com>
2017-10-01 19:15:58 +0530

    Exported the Mysql connection to config/db.js

---
+++ config/db.js
@@ -0,0 +1,20 @@
+﻿var mysql = require('mysql');
+var con = mysql.createConnection({
+    //Change this IP to localhost after development
+    host: "192.168.0.101",
+    user: "root",
+    password: "fuckkmea",
+    database: "voice"
+});
+
+con.connect(function (err) {
+    if (err)
+    {
+        console.log(err);
+        throw err;
+    }
+    console.log("Mysql Connection Successful");
+});
+
+
+module.exports = con;
