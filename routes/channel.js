ff3cfc9f1d0e5c68843eca8f282ccbea0c2e0ca4
Aslam Rasheed <aslampr07@outlook.com>
2017-10-02 18:17:01 +0530

    Added functions to create  Channel, optionally the discription of the channel can also be set.

--- routes/channel.js
+++ routes/channel.js
@@ -1,5 +1,6 @@
 ﻿var express = require('express');
 var token = require("../tools/auth")
+var mysql = require('mysql');
 
 module.exports = function (con) {
     var router = express.Router();
@@ -7,17 +8,36 @@ module.exports = function (con) {
 
     router.post('/create', function (req, res) {
         token.verify(req.query.auth, con, function (exist, id) {
-            if (exist)
-            {
-                res.send("The account does exist");
+            if (exist) {
+                if ('name' in req.body) {
+                    var data = { 'name': req.body.name, 'creationTime': new Date() };
+                    con.query("INSERT INTO Channel SET ?", data, function (err, result, field) {
+                        if (err)
+                            //Set the error management response.
+                            console.log(err);
+                        else {
+                            if ('description' in req.body) {
+                                var data = { 'body': req.body.description, 'channelID': result.insertId };
+                                var sql = "INSERT INTO Description SET ?";
+                                con.query(sql, data, function (err, results) {
+                                    if (err)
+                                        console.log(err);
+                                    res.send("Channel Created with discription");
+                                });
+                            }
+                            else
+                                res.send("Channel Created without disctiption");
+                        }
+                    });
+                }
             }
-            else
-            {
+            else {
+                res.status(401);
                 var response = {
                     'status': 'error',
                     'type': 'invalidAuth'
                 };
-                response.send(response);
+                res.send(response);
             }
         });
     });
@@ -25,5 +45,3 @@ module.exports = function (con) {
 
     return router;
 }
-
-//module.exports = router;
