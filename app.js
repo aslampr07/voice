a6f1d4964e21bb6a7539132079c78f06e7612928
Aslam Rasheed <aslampr07@outlook.com>
2017-10-28 22:21:42 +0530

    Some Big File changes

--- app.js
+++ app.js
@@ -7,14 +7,16 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json()); //may not need this line, as there is no json parsing.
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('webview'));
+
+//Using the static method for serving the static.
+app.use('/', express.static('public'));
 
 //For routing the channel requests.
 //The mysql connection has been passed to the routes.
-app.use('/account', require('./routes/account')(con));
-app.use('/channel', require('./routes/channel')(con));
-app.use('/profile', require('./routes/profile')(con));
-app.use('/post', require('./routes/post')(con));
+app.use('/api/1.0/account', require('./routes/account')(con));
+app.use('/api/1.0/channel', require('./routes/channel')(con));
+app.use('/api/1.0/profile', require('./routes/profile')(con));
+app.use('/api/1.0/post', require('./routes/post')(con));
 
 //The server is listening at port 8000, not using 80 because it need sudo for running the script, which is a
 //security risk. The port 80 is redirected to 8000 from the router.
