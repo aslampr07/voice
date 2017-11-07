e65fd8a6acb917c6bcff2e30cf478122900a1605
Aslam Rasheed <aslampr07@outlook.com>
2017-11-07 20:25:36 +0530

    Testing Visual Studio

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 ﻿var mysql = require('mysql');
 var con = mysql.createConnection({
     //Change this IP to localhost after development
-    host: "192.168.0.101",
+    host: "localhost",
     user: "root",
     password: "fuckkmea",
     database: "voice"
